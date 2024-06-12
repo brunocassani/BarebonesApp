53e5cc536bc4598cd4fa47de6162f06b55c35917
brunocassani <brunocassani04@gmail.com>
2024-06-11 21:27:24 -0400

    Initial Commit

---
+++ BarebonesApp/BarebonesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarebonesAppApp.swift
+//  BarebonesApp
+//
+//  Created by Bruno Cassani on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BarebonesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
